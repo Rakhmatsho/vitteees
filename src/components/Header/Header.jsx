7741aec328b194212a9d2bbfa2c371848b205bf2
Rakhmatsho <abdurasulovrahmatso@gmail.com>
2024-02-17 16:04:43 +0600

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import '../../style/Header.scss'
+
+const Header = () => {
+    return(
+        <header>
+            <nav>
+                <img src="" alt=""/>
+                <ul>
+                    <li className='cc'>About us</li>
+                    <li>Tags Samples</li>
+                    <li>Pricing</li>
+                    <li>Reviews</li>
+                    <li>FQA</li>
+                    <li>Contacts</li>
+                </ul>
+                <img src="" alt=""/>
+                <button>Get your tag</button>
+            </nav>
+            <section>
+
+            </section>
+        </header>
+    )
+}
+
+export default Header
